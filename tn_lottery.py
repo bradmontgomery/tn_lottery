c2b5c58999643bdd3cde9f7932d45fcccb34728a
Brad Montgomery <brad@bradmontgomery.net>
2013-01-08 11:36:58 -0600

    refactored into a Lottery class, added a powerball and mega_millions method, and renamed the module to tn_lottery

---
+++ tn_lottery.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+Generate random numbers for playing the TN lottery: http://www.tnlottery.com
+
+Disclaimer: You'll likely just lose your money.
+
+"""
+import random
+
+
+class Lottery(object):
+
+    def _choose(self, num, val_range):
+        """Randomly choose ``num`` values in the ``val_range`` range."""
+        vmin, vmax = val_range
+        return [str(random.randint(vmin, vmax)) for i in xrange(num)]
+
+    def powerball(self):
+        """Powerball - http://www.tnlottery.com/howtoplay/#power"""
+        values = self._choose(num=5, val_range=(1, 59))
+        powerball = self._choose(num=1, val_range=(1, 39))[0]
+        return "Powerball: {0} - PB {1}".format(', '.join(values), powerball)
+
+    def mega_millions(self):
+        """Mega Millions - http://www.tnlottery.com/howtoplay/#mega"""
+        values = self._choose(num=5, val_range=(1, 56))
+        megaball = self._choose(num=1, val_range=(1, 46))[0]
+        return "Mega Millions: {0} - Mega Ball: {1}".format(
+            ', '.join(values),
+            megaball
+        )
+
+
+if __name__ == "__main__":
+    # Prints randomly generated numbers for the different TN Lottery games.
+
+    lottery = Lottery()
+    print("\n-------------------\nTN Lottery Numbers!\n-------------------\n")
+    print(lotter.powerball())
+    print(lotter.mega_millions())

708e66c00f374b025c7575c89c2d8e8354bf495a
Brad Montgomery <brad@bradmontgomery.net>
2015-04-26 19:47:28 -0500

    cleaning this up and making it more correct: new winning numbers are picked every trial; plays for a single trial are checked against that number

--- tn_lottery/simluation.py
+++ tn_lottery/simluation.py
@@ -2,54 +2,92 @@
 # -*- coding: utf-8 -*-
 """
 
+Question: How much money & how much time would it take (for one person) to win
+the Powerball?
+
 Assumptions:
+
+- A single immortal is playing.
 - We're just playing the Powerball
-- We play every time possible (2-times a week)
+- We play every time possible (2-times a week; powerball is Wed & Sat)
 - We play 5 sets of numbers for $2 each ($10 / ticket)
-- We only "win" if we hit the jackpot (all numbers match)
--- todo: match minor wins.
+- We only "win" if we hit the jackpot (5 numbers match + the powerball number)
 
 """
 
-import sys
+import locale
 from lottery import Lottery
 
 COST = 2  # cost per play
 PLAYS = 5  # The number of sets we play for each ticket
+# Report progress every few years (52 weeks * 2 plays/week = 104 is 1 year)
+YEARS = 104 * 10
 
 
 def ticket(lotto, plays, cost):
+    """Generate a 'play ticket' consistent of numbers to play;
+
+    * lotto: An instance of the Lottery class.
+    * plays: The number of *plays* to generate (i.e. the set of numbers)
+    * cost: how much a single play costs.
+
+    Returns a tuple of consisting of 1) a list of powerball numbers, and 2)
+    the cost of playing.
+
+    """
     numbers = []
     for i in range(plays):
-        numbers.append(lotto.powerball)
+        numbers.append(lotto.powerball())
     return (numbers, plays * cost)
 
 
+def check(plays, winning_numbers):
+    """Check if the given plays match any of the winning numbers:
+
+    * plays is a list of powerball play tuples.
+    * winning_numbers is a powerball play tuple.
+
+    A powerball play tuple contains a list of 5 numbers followed by a
+    powerball number. e.g. ([6, 20, 21, 52, 55], 27)
+
+    """
+    return winning_numbers in plays
+
+
+def play(lotto):
+    plays, cost = ticket(lotto, PLAYS, COST)
+    winning_numbers = lotto.powerball()
+    return (check(plays, winning_numbers), cost)
+
+
+def report(trials, spent, won=False):
+    """Print how how many years have been played & how much money spent."""
+    if won:
+        print("YOU WON!")
+    years = int(trials/YEARS * 10)  # years grouped by 10s
+    cost = locale.currency(spent, grouping=True)
+    print("{0} Years: {1}".format(years, cost))
+
+
 def run():
-    spent = 0  # how much we've spent
-    ticket_numbers = []  # a list of possible lotto numbers
-    trials = 0  # Number of time's we've bought a ticket
+    locale.setlocale(locale.LC_ALL, '')  # Assume US because that's where TN is.
 
     lotto = Lottery()
-    winning_numbers = lotto.powerball()
+    spent = 0  # how much we've spent
+    trials = 0  # Number of time's we've bought a ticket
+    won = False
 
-    while winning_numbers not in ticket_numbers:
-        # Buy a new ticket
-        ticket_numbers, cost = ticket(lotto, PLAYS, COST)
-
-        # Track the cost & number of times we've played
+    while not won:
+        # Generate a ticket & a drawing
+        won, cost = play(lotto)
         spent += cost
         trials += 1
-        sys.stdout.write(".")
 
         # Print some info every year
-        if trials % 104 == 0:
-            years = trials / 104
-            sys.stdout.write("\n{0} Years, ${1}\n".format(years, spent))
+        if trials % YEARS == 0:
+            report(trials, spent, won)
 
-    sys.stdout.write("\n\nYOU WON!\n\n")
-    years = trials / 104
-    sys.stdout.write("\n{0} Years, ${1}\n".format(years, spent))
+    report(trials, spent, won)
 
 
 if __name__ == "__main__":

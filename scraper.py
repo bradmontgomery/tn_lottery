1ae10c1c5b528d0861742d529568c5cfc5af3631
Brad Montgomery <brad@bradmontgomery.net>
2019-07-29 14:21:11 -0500

    fixes

--- scraper.py
+++ scraper.py
@@ -31,6 +31,7 @@ class Parser(HTMLParser):
                 div.field-content.game-name
 
     """
+
     active = False
     which = None
     games = []
@@ -38,14 +39,14 @@ class Parser(HTMLParser):
 
     def handle_starttag(self, tag, attrs):
         classes = [val for attr, val in attrs if attr == "class"]
-        classes = ' '.join(classes).split()
+        classes = " ".join(classes).split()
 
-        if tag == "div" and 'prize-amount' in classes:
+        if tag == "div" and "prize-amount" in classes:
             self.active = True
-            self.which = 'amounts'
-        elif tag == "div" and 'game-name' in classes:
+            self.which = "amounts"
+        elif tag == "div" and "game-name" in classes:
             self.active = True
-            self.which = 'games'
+            self.which = "games"
 
     def handle_endtag(self, tag):
         if tag == "div":
@@ -70,14 +71,13 @@ def fetch_and_parse(page=0):
 
     content = None
     if resp.status_code == 200:
-        content = resp.content.decode('utf-8')
+        content = resp.content.decode("utf-8")
         parser.feed(content)
 
     return resp.status_code
 
 
 if __name__ == "__main__":
-
     page = 0
     while fetch_and_parse(page) == 200 and page < MAX_PAGES:
         print(f"Got page {page}...")
@@ -87,17 +87,15 @@ if __name__ == "__main__":
     data = parser.get_data()
 
     # Print some common stuff...
-    print("The Most commonly won games are:")
+    print("\nThe Most commonly won games are:")
     c = Counter([g for g, _ in data])
     for game, count in c.most_common(5):
         print(f"- {game} ({count} wins)")
 
-    print("The most commonly won amounts are:")
+    print("\nThe most commonly won amounts are:")
     c = Counter([amount for _, amount in data])
     for amount, count in c.most_common(5):
         print(f"- {amount} ({count} wins)")
-
-    return
 
     # TODO: ----- figure out how to parse amounts like: ----------------
     # - $1,000 a Week for Life
@@ -108,14 +106,20 @@ if __name__ == "__main__":
     # Which games pay out the best...
     results = []
     for game, amount in [(g, a.replace("$", "").replace(",", "")) for g, a in data]:
+        amount = amount.lower().strip()
+        if amount.endswith(" million"):
+            amount = float(amount.replace("million", "").strip()) * 1_000_000
+
         try:
-            amts = re.finall(r'\d+', amount)
-            if amts:
-                results.append((game, int(amts[0])))
+            # Replace anythign that doesn't look numeric.
+            amount = re.sub(r"[^\d+|\.]", "", str(amount))
+            if amount:
+                results.append((game, float(amount)))
         except (ValueError, AttributeError, TypeError) as err:
-            print(f"Unable to parse: {game}, {amount}")
+            print(f"Unable to parse {game}: '{amount}'")
             print(err)
-    data = sorted(results, lambda t: t[1])
+
+    data = sorted(results, key=lambda t: t[1])
 
     d = defaultdict(list)
     for game, amount in data:
@@ -125,7 +129,8 @@ if __name__ == "__main__":
     for game, values in d.items():
         m = statistics.mean(values)
         results.append((game, m))
-    results = sorted(results, lambda t: t[1])
-    print("The best-paying games on average are:")
+
+    results = sorted(results, key=lambda t: t[1])
+    print("\nThe best-paying games on average are:")
     for game, amount in results:
-        print(f"- {game}: ${amount:,}")
+        print(f"- {game}:\t ${int(amount):,}")
